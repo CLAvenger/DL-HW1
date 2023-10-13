18225085fa761cf80c8d9ddc2761e5f302786b22
Avenger <cl200424@mail.ustc.edu.cn>
2023-10-13 15:35:29 +0800

    由输入元素得到数据并成功展示在网页上

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask,render_template,request
+from utils import *
 app = Flask(__name__,static_url_path='', static_folder='templates')
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    symbol=''
     if request.method == 'POST':
         symbol = request.form.get('symbol')
-        print(symbol)
-    return render_template('index.html',symbol=symbol)
+        prop,compounds=get_data(symbol)
+        if not prop:
+            return render_template('index.html',error="没有这个元素")
+        return render_template('index.html',prop=prop,compounds=compounds)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.debug=True
--- utils.py
+++ utils.py
@@ -1,22 +1,27 @@
 import requests,json
-symbol=input("input the element symbol: ")
 def get_data(symbol):
     url="https://ptable.com/JSON/properties-90d5338.json"
     req=requests.get(url)
     data=json.loads(req.text)
     symbols=[data[i]['symbol'] for i in range(1,len(data))]
-
+    if symbol not in symbols:
+        return None,None
 
     prop=data[symbols.index(symbol)+1]
     weight=prop['weight']
     energy_levels=prop['electrons']
-    electronegativity=prop['electroneg']
+    prop['electrons']=str(prop['electrons'])[1:-1]
     melt_k=float(prop['melt'])
     boil_k=float(prop['boil'])
+    
     melt_c=melt_k-273.15
     boil_c=boil_k-273.15
     melt_f=melt_c*9/5+32
     boil_f=boil_c*9/5+32
+    prop['melt_c']="{:.5g}".format(melt_c)
+    prop['boil_c']="{:.5g}".format(boil_c)
+    prop['melt_f']="{:.5g}".format(melt_f)
+    prop['boil_f']="{:.5g}".format(boil_f)
     # print(melt_c,boil_c,melt_f,boil_f)
 
     url_compounds="https://ptable.com/JSON/compounds/formula="+symbol
@@ -34,3 +39,7 @@ def get_data(symbol):
     # print(compounds)
     return prop,compounds
 
+if __name__=="__main__":
+    symbol=input("input the element symbol: ")
+    prop,compounds=get_data(symbol)
+    print(prop)
